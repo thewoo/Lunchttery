3211bee0e82bb3002687541f931c1b0bef48c283
Jorge.Pardo <Jorge.Pardo@treenovum.es>
2012-11-27 13:08:22 +0100

    Initial Commit

---
+++ Lunchttery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lunchttery
+//
+//  Created by Jorge Pardo on 27/11/12.
+//  Copyright (c) 2012 Magic Dealers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lunchttery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lunchttery
+//
+//  Created by Jorge Pardo on 27/11/12.
+//  Copyright (c) 2012 Magic Dealers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
